b08813f0286af54ee3870a448e890db10aae1f3a
Misha Martin <90650953+MishaMartin@users.noreply.github.com>
2021-12-09 09:39:22 -0700

    Typo updated

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,11 +1,17 @@
 from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
+from dateutil.parser import parse
 
 class DeadlinedMetaReminder(ABCMeta, Iterable):
     @abstractmethod
     def is_due(self):
         pass
-class DeadlinedReminder(ABC, Iterable):
+class DateLinedReminder(ABC, Iterable):
     @abstractmethod
     def is_due(self):
         pass
+
+class DateReminder(DateLinedReminder):
+    def __init__(self, text, date):
+        self.parse(date) 
+        dayfirst=True
